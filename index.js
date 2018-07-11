31cee2f49273c335ba55853b931ea0042d24c6ad
Gary Baldwin <g11baldwin@gmail.com>
2018-07-11 17:35:46 -0400

    move index.js to appropriate place in module

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = require('./src/PPManager');
+module.exports = require('./ppsdk/src/PPManager');
 exports.printMsg = function() {
   console.log("This is a test message from the playPORTAL SDK package");
 }
