e904df504ad5a34e98eb5f0a1843d2b6d80d74a7
Gary Baldwin <g11baldwin@gmail.com>
2018-10-28 23:05:50 -0400

    Fix code working with ppAuth
    - ppAuth is used to store auth status, refresh token and access token

--- ppsdk/src/PPManager.js
+++ ppsdk/src/PPManager.js
@@ -29,10 +29,13 @@ export const PPgetBucketName = (isPrivate) =>  {  return _bucketName(isPrivate);
 export const PPcreateBucket = (bucketname, users, isPublic) => { return PPD.createBucket(bucketname, users, isPublic);}
 export const PPwriteData = (bucketname, k, v) =>  {  return PPData.writeBucket(bucketname, k, v);}
 export const PPreadData = (bucketname, k) =>  {  return PPData.readBucket(bucketname, k); }
-export const PPgetAccessToken = () => {  return ppAuth.accessToken; }
+export const PPgetAccessToken = () => { return ppAuth.accessToken; }
 export const PPgetRefreshToken = () => { return ppAuth.refreshToken; }
 export const PPgetEnvironment = () => { return environment; }
 export const PPlogout = () => { logout(); }
+export const PPgetProfileOrCoverImage = (isP) =>  {  return PPUser.getProfileOrCoverImage(isP); }
+export const PPgetProfileOrCoverImageURL = (isP) =>  {  return PPUser.getProfileOrCoverImageURL(isP); }
+export const PPgetFriendProfileImageURL = (imageId) => { return PPUser.getFriendProfileImageURL(imageId); }
 // ----------------------------------------------------------------------------
 // PPconfigure
 // Called to configure and start the sdk. If "userListener" is registered it
@@ -47,11 +50,12 @@ export const PPconfigure =  (id, sec, redir, env) => {
 
   getAuthPrefs()
     .then((response) => {
+      ppAuth = response
       if(ppAuth.status) {
-      refreshAccessToken((parms) => {
+        refreshAccessToken((parms) => {
         if((parms.refreshToken != "") && (parms.refreshToken != "unknown")) {
           PPUser.getProfile()
-            .then((response) => {
+          .then((response) => {
               ppUser = response;
               let bu=[];
               if(userListener) userListener(response, ppAuth.status);
@@ -74,29 +78,29 @@ export const PPconfigure =  (id, sec, redir, env) => {
 
               if(userListener) userListener(response, ppAuth.status);
               setAuthPrefs(ppAuth);
-            });
+          });
         } else {
           if(userListener) userListener(ppUser, ppAuth.status);
         }
       });
-    } else {
-      if(userListener) userListener(ppUser, ppAuth.status);
-    }
-  });
+      } else {
+        if(userListener) userListener(ppUser, ppAuth.status);
+      }
+    });
 }
 
 export const PPgetLoginRoute = () => {
-  return APIURLs.base(`oauth`) + '/signin?client_id=' + ppClient.id + '&redirect_uri=' + ppClient.redirectURI + '&state=beans' + '&response_type=implicit';
+  console.log("PPgetLoginRoute: ",  APIURLs.base(`oauth`) + '/signin?client_id=' + ppClient.id + '&redirect_uri=' + ppClient.redirectURI + '&state=beans' + '&response_type=implicit')
+    return APIURLs.base(`oauth`) + '/signin?client_id=' + ppClient.id + '&redirect_uri=' + ppClient.redirectURI + '&state=beans' + '&response_type=implicit';
 };
 export const PPaddUserListener = (u) => { userListener = u; };
 
 export const PPhandleOpenURL = (navigation) => {
-  const url = new URL(navigation.url, true)
   ppAuth.status = true;
-  ppAuth.accessToken = url.query['access_token'] || 'unknown'
-  ppAuth.refreshToken = url.query['refresh_token'] || 'unknown'
+  ppAuth.accessToken = navigation.getParam('access_token', 'unknown');
+  ppAuth.refreshToken = navigation.getParam('refresh_token', 'unknown');
   ppAuth.expirationTime = new Date();
-  if(url.query['expires_in'] === "1d") {
+  if(navigation.getParam('expires_in') === "1d") {
     ppAuth.expirationTime.setHours(ppAuth.expirationTime.getHours() + 12)
   } else {
     ppAuth.expirationTime.setHours(ppAuth.expirationTime.getHours() + 1)
@@ -179,8 +183,9 @@ const logout = () => { clearAuth(); };
 const getAuthPrefs = async () => {
   try {
     const result = await AsyncStorage.getItem('@auth')
-    const ppAuth = JSON.parse(result);
-    return ppAuth;
+    const auth = JSON.parse(result);
+    console.log("getAuthPrefs auth:", auth);
+    return auth;
   } catch (err) {
     console.error("getAuthPrefs error:" + err);
     clearAuth();
