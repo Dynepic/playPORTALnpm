266cb29ac4dfa227b7e5f6a2e58a9a9e0e9c8a26
Gary Baldwin <g11baldwin@gmail.com>
2018-10-28 23:03:46 -0400

    update to support image retrieval
    - uses get image by ID API

--- ppsdk/src/PPUserService.js
+++ ppsdk/src/PPUserService.js
@@ -2,10 +2,11 @@
 var axios = require('axios');
 var APIURLs = require('../utils/APIURLs');
 var PPRequest = require('../utils/request');
+import { PPgetAccessToken } from '../src/PPManager';
 
 // Disable logging
-var console = {};
-console.log = function(){};
+//var console = {};
+//console.log = function(){};
 
 export const getProfile = () => {
   return new Promise((resolve, reject) => {
@@ -23,6 +24,19 @@ export const getProfile = () => {
       });
 		})
 };
+
+
+// GET request for user's image
+export const getProfileOrCoverImageURL = (isProfile) => {
+  var tag = "cover"
+  if(isProfile) { tag = "picture" }
+  return APIURLs.User.my.profile + "/" + tag + "?authorization=Bearer " + PPgetAccessToken()
+}
+
+export const getFriendProfileImageURL = (imageId) => {
+  console.log("get friend image url:", APIURLs.Image.static + imageId + "?authorization=Bearer " + PPgetAccessToken());
+  return APIURLs.Image.static + imageId + "?authorization=Bearer " + PPgetAccessToken()
+}
 
 export const getFriendsProfiles = () => {
 	return new Promise((resolve, reject) => {
